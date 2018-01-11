22562a938d2d670499cf75444d408f9fb1c29bac
zhangzhangrp <995405279@qq.com>
2018-01-11 13:18:41 +0800

    1.11

--- 移动端/星巴克/js/index.js
+++ 移动端/星巴克/js/index.js
@@ -1,17 +1,19 @@
 $(function(){
     //menu
     $('#screen').css('height',$(window).height());
-    $('#menu').on('click',function(){  //显示
-        $('#slide-menu').css('display','block');
-        $('#screen').css('display','block');
+    $('#menu').on('click',function(){  
+            $('#slide-menu').css('display','block');
+            $('#screen-right').css('display','block');
+            $('#screen').css('display','block');
     });
   
-         $('#screen').on('click',function(e){   //点击右侧阴影处可使侧边栏消失
+         $('#screen-right').on('click',function(e){   //点击右侧阴影处可使侧边栏消失
               if($('#slide-menu').css('display')=='block'){
                 $('#slide-menu').css('display','none');
                 $(this).css('display','none');
+                $('#screen').css('display','none');
                }
-               e.stopPropagation();
+              
          } );
         //  $('#screen').trigger('click');
          window.onscroll= function(){    //滚动条滚动也能让侧边栏消失
